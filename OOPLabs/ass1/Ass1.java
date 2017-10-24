4f191621fded27cd58e12b4335294269a4a2181c
Emil Lozev <lozev@localhost.localdomain>
2017-10-24 10:31:47 +0100

    Final touches to the ass

--- OOPLabs/ass1/Ass1.java
+++ OOPLabs/ass1/Ass1.java
@@ -1,20 +1,53 @@
 import java.io.*;
 import java.util.Scanner;
-import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import javafx.scene.chart.*;
-import javafx.scene.Group;
 
 public class Ass1{
+
+	private static void printLettersAtTheBottom(){
+		for(int k = 0; k < 26; k++){
+				System.out.print((char)(k + 65) + " | ");
+			}
+		System.out.println("");
+	}
+
+	private static void printHistogram(int mostEcounteredLetter, int[] counters){
+		for(int j = mostEcounteredLetter; j > 0; j--){
+			for(int k = 0; k < 26; k++){
+				if(counters[k] >= j)
+					System.out.print("* | ");
+				else
+					System.out.print("  | ");
+			}
+			System.out.println(j);
+		}		
+	}
+
+	private static void printLettersWithCount(int[] counters){
+		for(int i = 0; i < 26; i++){
+				char letter = (char)(i + 65);
+				System.out.println(letter + ": " + counters[i]);
+		}
+	}
+
+	private static int findMostEcounteredLetter(int[] counters){
+		int mostEcounteredLetterCounter = 0;			
+		for(int i = 0; i < 26; i++)
+			if(mostEcounteredLetterCounter < counters[i])
+				mostEcounteredLetterCounter = counters[i];
+	
+		return mostEcounteredLetterCounter;
+	}
+
 	public static void main(String[] args){
 		String filename = "jc.txt";
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter path to file: ");
+		filename = scanner.nextLine();
+
 		File file = new File(filename); 
-		Scanner inputFile;
 		int[] counters = new int[27];
 
+		Scanner inputFile;
 		try{
 			inputFile = new Scanner(file);	
 			int lineCounter = 0;
@@ -33,23 +66,17 @@ public class Ass1{
 				}
 			}
 
-			for(int i = 0; i < 26; i++){
-				char letter = (char)(i + 65);
-				System.out.println(letter + ": " + counters[i]);
-			}
-			for(int i = 0; i < 26; i++){
-				char letter = (char)(i + 65);
+			
 
-				System.out.print(letter + ": ");
-				for(int j = 0; j < counters[i]; j++){
-					System.out.print('*');
-				}
-				System.out.println();
-			}
 
+			printLettersWithCount(counters);
+			printHistogram(findMostEcounteredLetter(counters), counters);
+			printLettersAtTheBottom();
+			
 			System.out.println(filename + " has " + lineCounter + " lines");
 			inputFile.close();
 		} catch (FileNotFoundException e){
+			System.out.println("File Found Exception!");
 			e.printStackTrace();
 		}
 	}
---
+++ OOPLabs/ass1/readme.txt
@@ -0,0 +1,7 @@
+Steps in running the program:
+1. javac Ass1.java
+2. javac Ass1
+3. The program ask for valid path to file to read
+4. The program outputs the result
+
+Java version used for this assignment: 1.8.0_144-b01
